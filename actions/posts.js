be9d6d6584ee2f55e08c40dfb87a68595abe6652
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-09 23:55:06 +0530

    :zap: :art: Cloudinary image added for creating new post

--- actions/posts.js
+++ actions/posts.js
@@ -1,36 +1,50 @@
 "use server";
+import { uploadImage } from "@/lib/cloudinary";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
 
 export async function createPost(prevState, formData) {
-  const title = formData.get('title');
-  const image = formData.get('image');
-  const content = formData.get('content');
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
 
   const errors = [];
 
   if (!title || title.trim().length === 0) {
-    errors.push("Title is required")
+    errors.push("Title is required");
   }
 
   if (!content || content.trim().length === 0) {
-    errors.push("Content is required")
+    errors.push("Content is required");
   }
 
   if (!image || image.size === 0) {
-    errors.push("Image is required")
+    errors.push("Image is required");
   }
 
   if (errors.length > 0) {
-    return { errors }
+    return { errors };
   }
 
-  await storePost({
-    imageUrl: '',
-    title,
-    content,
-    userId: 1
-  })
+  let imageUrl;
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error) {
+    throw new Error(
+      "Failed to upload the image, please try again after some time."
+    );
+  }
 
-  redirect('/feed')
+  try {
+    await storePost({
+      imageUrl,
+      title,
+      content,
+      userId: 1,
+    });
+  } catch (error) {
+    throw new Error("Failed to create new post");
+  }
+
+  redirect("/feed");
 }
