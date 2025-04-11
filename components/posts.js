894441cd89abbc4a20c78ac17c5ea0e0b6298fc9
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-04-12 00:00:54 +0530

    :zap: :art: image loader

--- components/posts.js
+++ components/posts.js
@@ -1,29 +1,48 @@
-'use client';
+"use client";
 
-import { formatDate } from '@/lib/format';
-import LikeButton from './like-icon';
-import { togglePostLikeStatus } from '@/actions/posts';
-import { useOptimistic } from 'react';
-import Image from 'next/image';
+import { formatDate } from "@/lib/format";
+import LikeButton from "./like-icon";
+import { togglePostLikeStatus } from "@/actions/posts";
+import { useOptimistic } from "react";
+import Image from "next/image";
+
+function imageLoader(config) {
+  const urlStart = config.src.split("upload/")[0];
+  const urlEnd = config.src.split("upload/")[1];
+  const transformations = `w_200,q_${config.quality}`;
+  const url = `${urlStart}upload/${transformations}/${urlEnd}`;
+  return url;
+}
 
 function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
-        <Image fill src={post.image} alt={post.title} />
+        <Image
+          loader={imageLoader}
+          // fill // due to warning we are adding height and width property but they are controlled by the css value
+          height={120}
+          width={200}
+          src={post.image}
+          alt={post.title}
+          quality={50}
+        />
       </div>
       <div className="post-content">
         <header>
           <div>
             <h2>{post.title}</h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
+              Shared by {post.userFirstName} on{" "}
               <time dateTime={post.createdAt}>
                 {formatDate(post.createdAt)}
               </time>
             </p>
           </div>
-          <form action={action.bind(null, post.id)} className={post.isLiked ? 'liked' : ''}>
+          <form
+            action={action.bind(null, post.id)}
+            className={post.isLiked ? "liked" : ""}
+          >
             <LikeButton />
           </form>
         </header>
@@ -34,22 +53,25 @@ function Post({ post, action }) {
 }
 
 export default function Posts({ posts }) {
+  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(
+    posts,
+    (prevState, updatedPostId) => {
+      const updatedPostIndex = prevState.findIndex(
+        (post) => post.id === updatedPostId
+      );
 
+      if (updatedPostIndex === -1) {
+        return prevState;
+      }
 
-  const [optimisticPosts, updateOptimisticPosts] = useOptimistic(posts, (prevState, updatedPostId) => {
-    const updatedPostIndex = prevState.findIndex(post => post.id === updatedPostId);
-
-    if (updatedPostIndex === -1) {
-      return prevState
+      const updatedPost = { ...prevState[updatedPostIndex] };
+      updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
+      updatedPost.isLiked = !updatedPost.isLiked;
+      const newPosts = [...prevState];
+      newPosts[updatedPostIndex] = { ...updatedPost };
+      return newPosts;
     }
-
-    const updatedPost = { ...prevState[updatedPostIndex] };
-    updatedPost.likes = updatedPost.likes + (updatedPost.isLiked ? -1 : 1);
-    updatedPost.isLiked = !updatedPost.isLiked;
-    const newPosts = [...prevState];
-    newPosts[updatedPostIndex] = { ...updatedPost };
-    return newPosts
-  })
+  );
 
   if (!optimisticPosts || optimisticPosts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
@@ -57,7 +79,7 @@ export default function Posts({ posts }) {
 
   async function updatedPost(postId) {
     updateOptimisticPosts(postId);
-    await togglePostLikeStatus(postId)
+    await togglePostLikeStatus(postId);
   }
 
   return (
